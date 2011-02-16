496581b56ca505b057ca4c3bdb35fc5bcaff225c
sugama <sugama@www.jamadam.com>
2011-02-17 00:58:56 +0900

    Document draft added

--- lib/Text/PSTemplate/Plugable.pm
+++ lib/Text/PSTemplate/Plugable.pm
@@ -5,45 +5,66 @@ use base qw(Text::PSTemplate);
 use Text::PSTemplate::PluginBase;
 
 our $VERSION = '0.01';
-	
-	sub plug {
-		
-		my ($self, $plug_array_ref) = (@_);
-		foreach my $plug_name (@{$plug_array_ref}) {
-			$plug_name->new($self);
-		}
-		$self->{pluged} = 1;
-	}
-	
-	sub set_namespace_base {
-		
-		my $self = shift;
-		$self->{namespace_base} = shift;
-	}
-	
-	sub set_default_plugin {
-		
-		my $self = shift;
-		$self->{default_plugin} = shift;
-	}
+    
+    sub plug {
+        
+        my ($self, $plug_array_ref) = (@_);
+        foreach my $plug_name (@{$plug_array_ref}) {
+            $plug_name->new($self);
+        }
+        $self->{pluged} = 1;
+    }
+    
+    sub set_namespace_base {
+        
+        my $self = shift;
+        $self->{namespace_base} = shift;
+    }
+    
+    sub set_default_plugin {
+        
+        my $self = shift;
+        $self->{default_plugin} = shift;
+    }
 1;
 
 __END__
 
 =head1 NAME
 
-Text::PSTemplate::Plugable - 
+Text::PSTemplate::Plugable - Plugable template engine
 
 =head1 SYNOPSIS
 
     use Text::PSTemplate::Plugable;
-    Text::PSTemplate::Plugable->new;
+    
+    my $tpl = tplText::PSTemplate::Plugable->new;
+    $tpl->plug(['MyPlug1', ...]);
+    $tpl->set_namespace_base('Foo::Bar');
+    $tpl->set_default_plugin('Foo::Bar');
+    $tpl->parse('...{%&say_hello_to('Kenji')%}...');
+    
+    package MyPlug1;
+	use strict;
+	use warnings;
+    use base qw(Text::PSTemplate::PluginBase);
+    sub say_hello_to : TplExport {
+        my ($plugin, $name) = (@_);
+        return "Hello $name";
+    }
 
 =head1 DESCRIPTION
 
+    Text::PSTemplate::Plugable Class is a sub class of Text::PSTemplate.
+    This extends some feature to Text::PSTemplate plugable.
+
 =head1 METHODS
 
-=head2 new
+=head2 plug
+
+=head2 set_namespace_base
+
+=head2 set_default_plugin
 
 =head1 AUTHOR
 
--- lib/Text/PSTemplate/PluginBase.pm
+++ lib/Text/PSTemplate/PluginBase.pm
@@ -9,131 +9,131 @@ use Class::C3;
 use base qw(Class::FileCacheable::Lite);
 
 our $VERSION = '0.01';
-	
-	### ---
-	### Constractor
-	### ---
-	sub new {
-		
-		my ($class, $tpl) = (@_);
-		no strict 'refs';
-		my $instance = \${$class. '::_instance'};
-		if (! blessed($$instance)) {
-			foreach my $pkg (@{$class. '::ISA'}) {
-				if ($pkg ne __PACKAGE__) {
-					$pkg->new($tpl);
-				}
-			}
-			$$instance = bless {ini => {}}, $class;
-		}
-		if (! $tpl->{pluged}) {
-				$$instance->_set_tpl_funcs($tpl);
-		}
-		return $$instance;
-	}
-	
-	### ---
-	### Get ini
-	### ---
-	sub ini {
-		
-		my ($self, $name) = (@_);
-		
-		if (exists $self->{ini}->{$name}) {
-			return $self->{ini}->{$name};
-		} else {
-			no strict 'refs';
-			for my $pkg (Class::C3::calculateMRO(ref $self)) {
-				if ($pkg eq __PACKAGE__) {
-					return undef;
-				}
-				my $ret = $pkg->new->{ini}->{$name};
-				if (defined $ret) {
-					return $ret;
-				}
-			}
-			return undef;
-		}
-	}
-	
-	### ---
-	### Set ini
-	### ---
-	sub set_ini {
-		
-		my ($self, $hash) = (@_);
-		$self->{ini} = $hash || {};
-		return $self;
-	}
-	
-	my %_tpl_exports = ();
-	
-	### ---
-	### Template function Attribute
-	### ---
-	sub TplExport : ATTR(CHECK) {
-		
-		my($pkg, $sym, undef, undef, undef, undef) = @_;
-		push(@{$_tpl_exports{$pkg}}, $sym);
-	}
-	
-	### ---
-	### Register template functions
-	### ---
-	sub _set_tpl_funcs {
-		
-		my ($self, $tpl) = (@_);
-		
-		my $plug_id = ref $self;
-		if (my $namespace_base = $tpl->{namespace_base}) {
-			$plug_id =~ s{$namespace_base\:\:}{};
-		}
+    
+    ### ---
+    ### Constractor
+    ### ---
+    sub new {
+        
+        my ($class, $tpl) = (@_);
+        no strict 'refs';
+        my $instance = \${$class. '::_instance'};
+        if (! blessed($$instance)) {
+            foreach my $pkg (@{$class. '::ISA'}) {
+                if ($pkg ne __PACKAGE__) {
+                    $pkg->new($tpl);
+                }
+            }
+            $$instance = bless {ini => {}}, $class;
+        }
+        if (! $tpl->{pluged}) {
+                $$instance->_set_tpl_funcs($tpl);
+        }
+        return $$instance;
+    }
+    
+    ### ---
+    ### Get ini
+    ### ---
+    sub ini {
+        
+        my ($self, $name) = (@_);
+        
+        if (exists $self->{ini}->{$name}) {
+            return $self->{ini}->{$name};
+        } else {
+            no strict 'refs';
+            for my $pkg (Class::C3::calculateMRO(ref $self)) {
+                if ($pkg eq __PACKAGE__) {
+                    return undef;
+                }
+                my $ret = $pkg->new->{ini}->{$name};
+                if (defined $ret) {
+                    return $ret;
+                }
+            }
+            return undef;
+        }
+    }
+    
+    ### ---
+    ### Set ini
+    ### ---
+    sub set_ini {
+        
+        my ($self, $hash) = (@_);
+        $self->{ini} = $hash || {};
+        return $self;
+    }
+    
+    my %_tpl_exports = ();
+    
+    ### ---
+    ### Template function Attribute
+    ### ---
+    sub TplExport : ATTR(CHECK) {
+        
+        my($pkg, $sym, undef, undef, undef, undef) = @_;
+        push(@{$_tpl_exports{$pkg}}, $sym);
+    }
+    
+    ### ---
+    ### Register template functions
+    ### ---
+    sub _set_tpl_funcs {
+        
+        my ($self, $tpl) = (@_);
+        
+        my $plug_id = ref $self;
+        if (my $namespace_base = $tpl->{namespace_base}) {
+            $plug_id =~ s{$namespace_base\:\:}{};
+        }
 
-		my $_tpl_exports = _get_tpl_exports(ref $self);
-		
-		foreach my $sym (@$_tpl_exports) {
-			
-			my $ref = \&$sym;
-			my $rapper = sub {
-				my $self = (blessed($_[0])) ? shift : $self;
-				my $ret = $self->$ref(@_);
-				return (defined $ret ? $ret : '');
-			};
-			
-			my $subname = ((scalar *$sym) =~ m{([^:]+$)})[0];
-			
-			### Set template funcs in template object
-			$tpl->set_func($plug_id. '::'. $subname => $rapper);
-			if (my $default_plugin = $tpl->{default_plugin}) {
-				if ($plug_id eq $default_plugin) {
-					$tpl->set_func($subname => $rapper);
-				} elsif ($plug_id =~ /^$default_plugin\::(.+)/) {
-					$tpl->set_func('::'. $1. '::'. $subname => $rapper);
-				}
-			}
-		}
-		
-		return $self;
-	}
-	
-	### ---
-	### Get template function entries
-	### ---
-	sub _get_tpl_exports {
-		
-		my $pkg = shift;
-		my @out = ();
-		no strict 'refs';
-		foreach my $super (@{$pkg. '::ISA'}) {
-			if ($super ne __PACKAGE__) {
-				push(@out, @{_get_tpl_exports($super)});
-			}
-		}
-		if (my $a = $_tpl_exports{$pkg}) {
-			push(@out, @$a);
-		}
-		return \@out;
-	}
+        my $_tpl_exports = _get_tpl_exports(ref $self);
+        
+        foreach my $sym (@$_tpl_exports) {
+            
+            my $ref = \&$sym;
+            my $rapper = sub {
+                my $self = (blessed($_[0])) ? shift : $self;
+                my $ret = $self->$ref(@_);
+                return (defined $ret ? $ret : '');
+            };
+            
+            my $subname = ((scalar *$sym) =~ m{([^:]+$)})[0];
+            
+            ### Set template funcs in template object
+            $tpl->set_func($plug_id. '::'. $subname => $rapper);
+            if (my $default_plugin = $tpl->{default_plugin}) {
+                if ($plug_id eq $default_plugin) {
+                    $tpl->set_func($subname => $rapper);
+                } elsif ($plug_id =~ /^$default_plugin\::(.+)/) {
+                    $tpl->set_func('::'. $1. '::'. $subname => $rapper);
+                }
+            }
+        }
+        
+        return $self;
+    }
+    
+    ### ---
+    ### Get template function entries
+    ### ---
+    sub _get_tpl_exports {
+        
+        my $pkg = shift;
+        my @out = ();
+        no strict 'refs';
+        foreach my $super (@{$pkg. '::ISA'}) {
+            if ($super ne __PACKAGE__) {
+                push(@out, @{_get_tpl_exports($super)});
+            }
+        }
+        if (my $a = $_tpl_exports{$pkg}) {
+            push(@out, @$a);
+        }
+        return \@out;
+    }
 
 1;
 
@@ -145,14 +145,34 @@ Text::PSTemplate::PlugBase - Plugin Abstract Class
 
 =head1 SYNOPSIS
 
-    use Text::PSTemplate::PlugBase;
-    Text::PSTemplate::PlugBase->new;
+    package MyPlug1;
+    use strict;
+    use warnings;
+    use base qw(Text::PSTemplate::PluginBase);
+    
+    sub say_hello_to : TplExport {
+        my ($plugin, $name) = (@_);
+        return "Hello $name";
+        
+        my $value = $plugin->ini('some_key');
+    }
 
 =head1 DESCRIPTION
+
+This is an Abstract Class whitch represents plugins for
+Text::PSTemplate::Plugable Class.
 
 =head1 METHODS
 
 =head2 new
+
+=head2 set_ini
+
+=head2 ini
+
+=head1 ATTRIBUTE
+
+=head2 TplExport
 
 =head1 AUTHOR
 
