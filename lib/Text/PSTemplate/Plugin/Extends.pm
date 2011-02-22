ba4f2f1c1ee30ebbbc8b7e3de56eb1901545435c
sugama <sugama@www.jamadam.com>
2011-02-22 20:43:19 +0900

    untabify

--- lib/Text/PSTemplate/Plugin/Extends.pm
+++ lib/Text/PSTemplate/Plugin/Extends.pm
@@ -13,9 +13,9 @@ our $VERSION = '0.01';
         
         my ($self, $file) = @_;
         my $tpl = Text::PSTemplate::Plugable->new;
-		$tpl->plug('Text::PSTemplate::Plugin::Extends::_Sub', '');
+        $tpl->plug('Text::PSTemplate::Plugin::Extends::_Sub', '');
         $tpl->parse(Text::PSTemplate::inline_data(0));
-		return $tpl->parse_file($file);
+        return $tpl->parse_file($file);
     }
 
 package Text::PSTemplate::Plugin::Extends::_Sub;
@@ -31,23 +31,23 @@ use Text::PSTemplate;
         
         my ($self, $name) = @_;
         my $tpl = Text::PSTemplate->new;
-		$tpl->mother->set_var($name => $tpl->parse(Text::PSTemplate::inline_data(0)));
-		return;
+        $tpl->mother->set_var($name => $tpl->parse(Text::PSTemplate::inline_data(0)));
+        return;
     }
-	
+    
     ### ---
     ### placeholder specification
     ### ---
-	sub placeholder : TplExport {
+    sub placeholder : TplExport {
         
         my ($self, $name) = @_;
         my $tpl = Text::PSTemplate->new;
-		if (my $val = $tpl->var($name)) {
-			return $val;
-		} else {
-			return $tpl->parse(Text::PSTemplate::inline_data(0));
-		}
-	}
+        if (my $val = $tpl->var($name)) {
+            return $val;
+        } else {
+            return $tpl->parse(Text::PSTemplate::inline_data(0));
+        }
+    }
 
 1;
 
@@ -60,41 +60,41 @@ Text::PSTemplate::Plugin::Extends - Port of Extends syntax from Django
 
 =head1 SYNOPSIS
 
-	base.html
-	
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<link rel="stylesheet" href="style.css" />
-		<title><% &placeholder('title')<<DEFULT %>My amazing site<% DEFULT %></title>
-	</head>
-	
-	<body>
-		<div id="sidebar">
-			<% &placeholder('sidebar')<<DEFULT %>
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/blog/">Blog</a></li>
-			</ul>
-			<% DEFULT %>
-		</div>
-	
-		<div id="content">
-			<% &placeholder('content')<<DEFULT %><% DEFULT %>
-		</div>
-	</body>
-	</html>
-	
-	child.html
-	
-	<% &extends('base.html')<<EXTENDS %>
-		<% &block('title')<<BLOCK %>My amazing blog<% BLOCK %>
-		<% &block('content')<<BLOCK %><% &each($blog_entries, 'entry')<<ENTRIES %>
-			<h2><% $entry->{title} %></h2>
-			<p><% $entry->{body} %></p>
-		<% ENTRIES %><% BLOCK %>
-	<% EXTENDS %>
+    base.html
+    
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <link rel="stylesheet" href="style.css" />
+        <title><% &placeholder('title')<<DEFULT %>My amazing site<% DEFULT %></title>
+    </head>
+    
+    <body>
+        <div id="sidebar">
+            <% &placeholder('sidebar')<<DEFULT %>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog/">Blog</a></li>
+            </ul>
+            <% DEFULT %>
+        </div>
+    
+        <div id="content">
+            <% &placeholder('content')<<DEFULT %><% DEFULT %>
+        </div>
+    </body>
+    </html>
+    
+    child.html
+    
+    <% &extends('base.html')<<EXTENDS %>
+        <% &block('title')<<BLOCK %>My amazing blog<% BLOCK %>
+        <% &block('content')<<BLOCK %><% &each($blog_entries, 'entry')<<ENTRIES %>
+            <h2><% $entry->{title} %></h2>
+            <p><% $entry->{body} %></p>
+        <% ENTRIES %><% BLOCK %>
+    <% EXTENDS %>
 
 =head1 DESCRIPTION
 
--- t/03_PST_Plugable_Extends.t
+++ t/03_PST_Plugable_Extends.t
@@ -8,21 +8,21 @@ use Data::Dumper;
     
     __PACKAGE__->runtests;
     
-	sub basic : Test {
-		
+    sub basic : Test {
+        
         my $tpl = Text::PSTemplate::Plugable->new();
-		$tpl->plug('Text::PSTemplate::Plugin::Extends','');
-		
-		$tpl->set_var(blog_entries => [
-			{title => 'Entry one', body => 'This is my first entry.'},
-			{title => 'Entry two', body => 'This is my second entry.'},
-		]);
-		
-		my $parsed = $tpl->parse_file('t/template/03_PST_Plugable_Extends2.txt');
-		my $expected = $tpl->get_file('t/template/03_PST_Plugable_Extends3.txt')->content;
-		is(compress_html($parsed), compress_html($expected));
-	}
-	
+        $tpl->plug('Text::PSTemplate::Plugin::Extends','');
+        
+        $tpl->set_var(blog_entries => [
+            {title => 'Entry one', body => 'This is my first entry.'},
+            {title => 'Entry two', body => 'This is my second entry.'},
+        ]);
+        
+        my $parsed = $tpl->parse_file('t/template/03_PST_Plugable_Extends2.txt');
+        my $expected = $tpl->get_file('t/template/03_PST_Plugable_Extends3.txt')->content;
+        is(compress_html($parsed), compress_html($expected));
+    }
+    
     sub compress_html {
         
         my $sql = shift;
